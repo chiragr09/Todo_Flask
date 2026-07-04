52f20c8f56a36ed1a4fdf2a19c4eb37838f371c8
Garihc <chiragramanna@gmail.com>
2026-07-05 00:36:10 +0530

    fix: Import CORS for integration with frontend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from config import Config
+from flask_cors import CORS
 from app.database.db import initialize_db
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 
 app.config["MONGO_URI"] = "mongodb://localhost/TodoApp"
